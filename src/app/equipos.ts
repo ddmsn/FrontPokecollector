0bb2a1c2a53e24f6c9aeb25a9d5ae8cab58ec3ff
Cristopher <caescalante@ccoo.es>
2024-05-08 13:06:03 +0200

    primer intento equipos

---
+++ src/app/equipos.ts
@@ -0,0 +1,11 @@
+export class Equipos {
+  num:number;
+  idUser:number;
+  nombre:string;
+  pokemon1_id:number;
+  pokemon2_id:number;
+  pokemon3_id:number;
+  pokemon4_id:number;
+  pokemon5_id:number;
+  pokemon6_id:number;
+}
